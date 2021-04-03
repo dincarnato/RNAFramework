2170d7c95c989a582148a6e59023f4e9c17a8d4f
Danny Incarnato <d.incarnato@rug.nl>
2021-04-04 00:37:25 +0200

    RNA Framework v2.7.1

--- lib/RF/Config.pm
+++ lib/RF/Config.pm
@@ -19,20 +19,20 @@ our (%scoremethods, %normmethods);
                  "3" => "Box-plot" );
 
 sub new {
-    
+
     my $class = shift;
     my %parameters = @_ if (@_);
-    
+
     if (exists $parameters{file} &&
         -e $parameters{file}) {
-        
+
         eval { Config::Simple->import_from($parameters{file}, \%parameters); } or Core::Utils::throw("Malformed configuration file");
-        
+
         # This removes the leading 'default.' prefix added by Config::Simple when parsing 'key=value' pairs
-        %parameters = map { lc(s/^default.//r) => $parameters{$_} } keys(%parameters); 
-        
-    } 
-    
+        %parameters = map { lc(s/^default.//r) => $parameters{$_} } keys(%parameters);
+
+    }
+
     my $self = $class->SUPER::new(%parameters);
     $self->_init({ file              => undef,
                    scoremethod       => "Ding",
@@ -49,25 +49,25 @@ sub new {
                    maxuntreatedmut   => 0.05,
                    maxmutationrate   => 0.2,
                    raw               => 0,
-                   autowrite         => 1 }, \%parameters); 
-    
+                   autowrite         => 1 }, \%parameters);
+
     $self->_validate();
     $self->_fixproperties();
-    
+
     $self->write() if (defined $self->{file} &&
                        !-e $self->{file} &&
                        $self->{autowrite});
-    
+
     return($self);
-    
+
 }
 
 sub _validate {
-    
+
     my $self = shift;
 
     $self->{normwindow} = $self->{windowoffset} if ($self->{windowoffset} > $self->{normwindow});
-    
+
     $self->throw("Invalid scoreMethod value") if ($self->{scoremethod} !~ m/^Ding|Rouskin|Siegfried|Zubradt|[1234]$/i);
     $self->throw("Invalid normMethod value") if ($self->{normmethod} !~ m/^(2-8\%|90\% Winsorising|Box-?plot|[123])$/i);
     $self->throw("2-8% normalization cannot be used with Rouskin scoring method") if ($self->{scoremethod} =~ m/^(Rouskin|2)$/i &&
@@ -93,23 +93,23 @@ sub _validate {
     $self->throw("Invalid maxUntreatedMut value") if (!ispositive($self->{maxuntreatedmut}));
     $self->throw("maxUntreatedMut value should be comprised between 0 and 1") if (!inrange($self->{maxuntreatedmut}, [0, 1]));
     $self->throw("maxMutationRate value should be comprised between 0 and 1") if (!inrange($self->{maxmutationrate}, [0, 1]));
-    
+
 }
 
 sub _fixproperties {
-    
+
     my $self = shift;
-    
+
     if ($self->{scoremethod} =~ m/^Ding|1$/i) { $self->{scoremethod} = 1; }
     elsif ($self->{scoremethod} =~ m/^Rouskin|2$/i) { $self->{scoremethod} = 2; }
     elsif ($self->{scoremethod} =~ m/^Siegfried|3$/i) { $self->{scoremethod} = 3; }
     elsif ($self->{scoremethod} =~ m/^Zubradt|4$/i) { $self->{scoremethod} = 4; }
-    
+
     $self->{normmethod} = $self->{normmethod} =~ m/^2-8\%|1$/ ? 1 : ($self->{normmethod} =~ m/^(90\% Winsorising|2)$/i ? 2 : 3);
-    $self->{reactivebases} = $self->{reactivebases} =~ m/^all$/i ? "ACGT" : join("", sort(uniq(iupac2nt(rna2dna($self->{reactivebases})))));
+    $self->{reactivebases} = $self->{reactivebases} =~ m/^all$/i ? "ACGT" : join("", sort(uniq(split("", join("", iupac2nt(rna2dna($self->{reactivebases})))))));
     $self->{normindependent} = $self->{normindependent} =~ m/^TRUE|yes|1$/i ? 1 : 0;
     $self->{remapreactivities} = $self->{remapreactivities} =~ m/^TRUE|yes|1$/i ? 1 : 0;
-    
+
 }
 
 sub scoremethod { return($_[1] ? $scoremethods{$_[0]->{scoremethod}} : $_[0]->{scoremethod}); }
@@ -139,32 +139,32 @@ sub maxuntreatedmut { return($_[0]->{maxuntreatedmut}); }
 sub maxmutationrate { return($_[0]->{maxmutationrate}); }
 
 sub summary {
-    
+
     my $self = shift;
-    
+
     my $table = Term::Table->new(indent => 2);
     $table->head("Parameter", "Value");
     $table->row("Scoring method", $self->scoremethod(1));
     $table->row("Normalization method", $self->normmethod(1));
-    
+
     if ($self->{scoremethod} == 1) { # Ding
-        
+
         $table->row("Pseudocount", $self->{pseudocount});
         $table->row("Maximum score", $self->{maxscore});
-        
+
     }
     elsif ($self->{scoremethod} == 3) { # Siegfried
-        
+
         $table->row("Maximum untreated sample mutation rate", $self->{maxuntreatedmut});
-        
+
     }
-    
+
     if ($self->{scoremethod} =~ m/^[34]$/) { # Siegfried/Zubradt
-        
+
         $table->row("Maximum mutation rate", $self->{maxmutationrate});
-        
+
     }
-    
+
     $table->row("Remap reactivities", ($self->{remapreactivities} ? "Yes" : "No"));
     $table->row("Normalization window", ($self->{normwindow} >= 1e5 ? sprintf("%.2e", $self->{normwindow}) : $self->{normwindow}));
     $table->row("Window sliding offset", ($self->{normwindow} >= 1e5 ? sprintf("%.2e", $self->{windowoffset}) : $self->{windowoffset}));
@@ -173,42 +173,42 @@ sub summary {
     $table->row("Minimum mean coverage", $self->{meancoverage});
     $table->row("Minimum median coverage", $self->{mediancoverage});
     $table->blank();
-    
+
     print "\n[+] Configuration summary:\n\n";
     $table->print();
-    
+
 }
 
 sub write {
-    
+
     my $self = shift;
     my $file = shift || $self->{file};
-    
+
     return if (!defined $file);
-    
+
     open(my $fh, ">", $file) or $self->throw("Unable to write configuration file \"" . $file . "\" (" . $! . ")");
-    
+
     print $fh "scoreMethod=" . $self->scoremethod(1) . "\n";
     print $fh $self->{raw} ? "raw=yes\n" : "normMethod=" . $self->normmethod(1) . "\n";
-    
+
     if ($self->{scoremethod} == 1) { # Ding
-        
+
         print $fh "pseudoCount=" . $self->{pseudocount} . "\n" .
                   "maxScore=" . $self->{maxscore} . "\n";
-        
+
     }
     elsif ($self->{scoremethod} == 3) { # Siegfried
-        
+
         print $fh "maxUntreatedMut=" . $self->{maxuntreatedmut} . "\n";
-        
+
     }
-    
+
     if ($self->{scoremethod} =~ m/^[34]$/) { # Siegfried/Zubradt
-        
+
         print $fh "maxMutationRate=" . $self->{maxmutationrate} . "\n";
-        
+
     }
-    
+
     print $fh "remapReactivities=" . $self->{remapreactivities} . "\n" .
               "normWindow=" . $self->{normwindow} . "\n" .
               "windowOffset=" . $self->{windowoffset} . "\n" .
@@ -216,9 +216,9 @@ sub write {
               "normIndependent=" . ($self->{normindependent} ? "yes" : "no") . "\n" .
               "meanCoverage=" . $self->{meancoverage} . "\n" .
               "medianCoverage=" . $self->{mediancoverage} . "\n";
-              
+
     close($fh);
-    
+
 }
 
 1;
